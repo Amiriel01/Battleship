57641fd6f6792cb0cda331dcf5a1409d0ed4413b
Sarah Quinney <quinney.sarah@gmail.com>
2023-07-26 15:20:59 -0500

    moved orientation and play again buttons to domManager

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -1,10 +1,41 @@
+import { player } from "./player";
+
+let instance = null;
+
+export default () => {
+    if (instance !== null) {
+        return instance;
+    }
+
+    instance = {
+        
+        bindPlayAgainButton: function () {
+            document.querySelector("#play-again").addEventListener("click", function () {
+                document.location.reload();
+            })
+        },
+
+        bindOrientationButton: function () {
+            let button = document.querySelector("#ship-orientation");
+            button.addEventListener("click", () => {
+                this.flipOrientation();
+                button.innerText = this.orientation;
+            });
+        },
+
+        flipOrientation: function () {
+            if (this.orientation === "Horizontal") {
+                this.orientation = "Vertical";
+            } else {
+                this.orientation = "Horizontal";
+            }
+        },
 
 
-function bindPlayAgainButton() {
-    document.querySelector("#play-again").addEventListener("click", function () {
-        document.location.reload();
-    })
-
+        // export { 
+        //     bindPlayAgainButton, 
+        //     bindOrientationButton
+        // }
+    }
+    return instance
 }
-
-export { bindPlayAgainButton }
